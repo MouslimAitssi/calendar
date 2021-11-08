476128e3c8fdca158a56e76ee9656fd16426b820
Mouslim Aitssi <mouslimaitssi@gmail.com>
2021-11-08 21:21:06 +0100

    select all | colored cell

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -6,42 +6,88 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { days, rows } from '../shared/constants';
-import CustomTableCell from './CustomTableCell';
+import { NUMBER_OF_DAYS, rows, weekends } from '../shared/constants';
 
 
 export default function BasicTable() {
-
   function fillValuesWithZeros() {
     let vals = [];
     rows.map((row) => {
       let arr = []
-      days.map((day) => {
-        arr.push(0);
-      })
+      for (let i = 1; i < NUMBER_OF_DAYS; i++) {
+        arr.push(0)
+      }
       vals.push([row, arr]);
     });
     return vals;
   }
   
   function getValue(row, day) {
-    for (let index = 0; index < values.length; index++) {
-      if(values[index][0] === row) {
-        if(values[index][1][day - 1] !==0) {// Modification ajoutée pour afficher le vide au lieu des zéros
-          return values[index][1][day - 1];
+    if(!isWeekend(day)) {
+      for (let index = 0; index < values.length; index++) {
+        if(values[index][0] === row) {
+          if(values[index][1][day - 1] !==0) {
+            return values[index][1][day - 1];
+          }
+          // Rendering blank rather than 0
+          return '';
         }
-        return '';
       }
+
+      return '';
     }
   }
 
-
+  function nextValue(prevValue) {
+    switch (prevValue) {
+      case 0:
+        return 1;
+      case 1:
+        return 0.5;
+      case 0.5:
+        return 1.5;
+      case 1.5:
+        return 0;
+      default:
+        return 0;
+    }
+  }
   
   function toggle(row, day) {
+      let vals = Object.create(values);
+      for (let index = 0; index < vals.length; index++) {
+        if(vals[index][0] === row) {
+          vals[index][1][day - 1] = nextValue(vals[index][1][day - 1]);
+          break;
+        }
+      }
+      setValues(vals);
+  }
+
+  function isWeekend(day) {
+    if (!weekends.includes(day)) {
+      return false;
+    }
+    return true;
+  }
+
+  const days = [];
+  for (let i = 1; i < NUMBER_OF_DAYS; i++) {
+    days.push(i);
+  }
+
+  function selectAll(row) {
     let vals = Object.create(values);
     for (let index = 0; index < vals.length; index++) {
       if(vals[index][0] === row) {
-        vals[index][1][day - 1] = (vals[index][1][day - 1] + 0.5)%2;
+        for (let i = 0; i < NUMBER_OF_DAYS; i++) {
+          if(!(isWeekend(vals[index][1][i - 1]) === '')) {
+            vals[index][1][i - 1] = 1;
+          }
+          else {
+            vals[index][1][i - 1] = 0;
+          }
+        }
         break;
       }
     }
@@ -61,6 +107,7 @@ export default function BasicTable() {
               {days.map((day) => (
               <TableCell align="right" key={day}>{day}</TableCell>
               ))}
+              <TableCell>Sélectionner tous</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -74,9 +121,10 @@ export default function BasicTable() {
                 </TableCell>
                 {days.map((day) => {
                   return (
-                    <CustomTableCell key={day + row} toggle={toggle} row={row} day={day} value={getValue(row, day)}/>
+                    <TableCell key={day + row} className={(isWeekend(day) ? "gray-cell" : '') + " " + (getValue(row, day) !== ''&&!isWeekend(day) ? 'selected-cell' : '')} align="right" onClick={()=>toggle(row, day)}>{getValue(row, day)}</TableCell>
                   )
                 })}
+                <TableCell key={row + "selectall"} onClick={()=>selectAll(row)}></TableCell>
               </TableRow>
             ))}
           </TableBody>
